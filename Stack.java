7e7030fc0761c07b60ae2e7fb015906b5d29980f
WasanNachai <149573377+WasanNachai@users.noreply.github.com>
2024-06-07 14:29:14 +0000

    Chang code

---
+++ Stack.java
@@ -0,0 +1,30 @@
+import java.util.stack;
+
+public class FromFill{
+
+    private final static int CAPASITY = 10 ;
+    private int[] data; 
+    private int size = 0;
+
+    //constructer with default capacity
+    public FromFill(){
+        data = new int [CAPASITY];
+    }
+
+    public FromFill (int capacity){
+        data = new int[capacity];
+    }
+
+    public getSize (){
+        return size;
+    }
+
+    
+    
+   
+
+
+
+
+    
+} 
